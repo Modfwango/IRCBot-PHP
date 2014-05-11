407b84aae03dd763f04217c79af7a135e21d0d1f
Clay Freeman <git@clayfreeman.com>
2014-05-11 18:21:46 -0500

    Added dependency handling!

--- includes/moduleManagement.php
+++ includes/moduleManagement.php
@@ -1,6 +1,26 @@
 <?php
   class ModuleManagement {
     private static $modules = array();
+    private static $waitingList = array();
+
+    public static function checkDependencies($module) {
+      if (isset($module->depend) && is_array($module->depend)
+          && count($module->depend) > 0) {
+        $depend = $module->depend;
+        foreach ($depend as $key => $d) {
+          if (self::isLoaded($d)) {
+            unset($depend[$key]);
+          }
+        }
+        if (count($depend) == 0) {
+          return true;
+        }
+      }
+      else {
+        return true;
+      }
+      return false;
+    }
 
     public static function isLoaded($name) {
       foreach (self::$modules as $module) {
@@ -42,11 +62,25 @@
         eval($eval);
         if (class_exists($classname)) {
           $module = new $classname();
-          if (is_object($module) && method_exists($module, "isInstantiated")
-              && $module->isInstantiated()) {
-            self::$modules[] = $module;
-            Logger::info("Loaded module \"".$name."\"");
-            return true;
+          if (is_object($module) && method_exists($module, "isInstantiated")) {
+            if (self::checkDependencies($module)) {
+              if ($module->isInstantiated()) {
+                self::$modules[] = $module;
+                Logger::info("Loaded module \"".$name."\"");
+                foreach (self::$waitingList as $item) {
+                  if (self::checkDependencies($item)) {
+                    self::$modules[] = $item[1];
+                    Logger::info("Loaded module \"".$item[0]."\"");
+                  }
+                }
+                return true;
+              }
+            }
+            else {
+              self::$waitingList[] = array($name, $module);
+              Logger::info("Deferring load of module \"".$name."\"");
+              return null;
+            }
           }
           else {
             Logger::info("Unable to load module \"".$name."\"");
@@ -85,6 +119,13 @@
       if (self::isLoaded(basename($name))) {
         foreach (self::$modules as $key => $module) {
           if (strtolower($module->name) == strtolower(basename($name))) {
+            foreach (self::$modules as $m) {
+              if (isset($m->depend) && is_array($m->depend)
+                  && count($m->depend) > 0
+                  && in_array(strtolower(basename($name)), $m->depend)) {
+                self::unloadModule($m->name);
+              }
+            }
             EventHandling::unregisterModule($module);
             unset(self::$modules[$key]);
             Logger::info("Unloaded module \"".$name."\"");
