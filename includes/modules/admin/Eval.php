82ec63ba011bc1e7a8c86625d15fe418b541de5b
Clay Freeman <git@clayfreeman.com>
2014-05-31 19:43:03 -0500

    Added an Eval module.

---
+++ includes/modules/admin/Eval.php
@@ -0,0 +1,42 @@
+<?php
+  class @@CLASSNAME@@ {
+    public $depend = array("ChannelMessageEvent", "UserIdentification");
+    public $name = "Eval";
+    private $queue = array();
+
+    public function receiveChannelMessage($name, $data) {
+      $connection = $data[0];
+      $source = $data[1];
+      $target = $data[2];
+      $message = $data[3];
+
+      $ex = explode(" ", $message);
+      if (preg_match("/^".$connection->getNickname().". eval (.+)/i", $message,
+          $matches)) {
+        $module = ModuleManagement::getModuleByName("UserIdentification");
+        if (is_object($module)) {
+          $this->queue[$module->testLogin($connection, $this,
+            "userLoginCallback", $source[0])] = array($source[0], $target,
+            $matches[1]);
+        }
+      }
+    }
+
+    function userLoginCallback($connection, $id, $nick, $loggedin) {
+      $entry = $this->queue[$id];
+      if ($loggedin == true) {
+        $connection->send("PRIVMSG ".$entry[1]." :".eval($entry[2]));
+      }
+      else {
+        $connection->send("NOTICE ".$entry[0].
+          " :You are not authorized to use this command.");
+      }
+    }
+
+    public function isInstantiated() {
+      EventHandling::registerForEvent("channelMessageEvent", $this,
+        "receiveChannelMessage");
+      return true;
+    }
+  }
+?>
